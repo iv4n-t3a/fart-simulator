ad1d2d88f5adfaa9c72d5b08011bb705c0d599d8
iv4n-t3a <iv4n.t3a@yandex.ru>
2025-04-12 18:37:39 +0300

    Remove scenes directory from root

--- cmd/fart-simulator/main.go
+++ cmd/fart-simulator/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/iv4n-t3a/fart-simulator/scenes"
+import (
+	"github.com/iv4n-t3a/fart-simulator/internal/metrics/observers"
+	"github.com/iv4n-t3a/fart-simulator/internal/simulation"
+)
+
+func RunSimpleSimulation() {
+	colCounter := observers.NewCollisionCounterObserver()
+	colContCounter := observers.NewCollisionWithContainerCounterObserver()
+
+	simulationInst := simulation.NewSingleChunkSimulation(100)
+	simulationInst.SubscribeCollision(colCounter)
+	simulationInst.SubscribeCollisionWithContainer(colContCounter)
+	simulationInst.AddReporter(colCounter)
+	simulationInst.AddReporter(colContCounter)
+
+	simulationInst.Run(1)
+	simulationInst.ReportMetrics()
+}
 
 func main() {
-	scenes.RunSimpleScene()
+	RunSimpleSimulation()
 }
