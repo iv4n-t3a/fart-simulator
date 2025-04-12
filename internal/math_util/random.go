c8ac3394e9ce33473c303a8ba0e2cbca01586fa2
Ivan Anishchenko <ivananisenko08@gmail.com>
2025-04-12 18:18:29 +0300

    Fix RandomFloat function

--- internal/math_util/random.go
+++ internal/math_util/random.go
@@ -3,5 +3,5 @@ package math_util
 import "math/rand"
 
 func RandFloat(min float64, max float64) float64 {
-	return min + rand.Float64()*(max-min)*rand.Float64()
+	return min + rand.Float64()*(max-min)
 }
